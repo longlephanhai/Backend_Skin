0261f09ee33aa84d1b70dee9e043d1c217ec32ee
longlephanhai <longlph.23it@gmail.com>
2026-04-12 14:19:00 +0700

    register user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
+import { Public, ResponseMessage } from 'src/decorator/customize';
+import { IUser } from 'src/types';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
     @UseGuards(LocalAuthGuard)
+    @Public()
     @Post('login')
     async login(@Request() req) {
         return this.authService.login(req.user);
+    }
+
+    @Public()
+    @ResponseMessage('Register successfully')
+    @Post('register')
+    async register(@Body() registerUser: CreateUserDto) {
+        return this.authService.register(registerUser);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
 
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { IUser } from 'src/types';
-
+import bcrypt from "bcryptjs";
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 @Injectable()
 export class AuthService {
     constructor(
@@ -12,7 +13,7 @@ export class AuthService {
     ) { }
 
     async validateUser(email: string, password: string) {
-        const user = await this.usersService.findOne(email);
+        const user = await this.usersService.findByEmail(email);
         if (user && user.password === password) {
             const { password, ...result } = user;
             return result;
@@ -26,5 +27,20 @@ export class AuthService {
             access_token: this.jwtService.sign(payload),
         };
     }
+
+    async register(user: CreateUserDto) {
+        const isExist = await this.usersService.findByEmail(user.email);
+        if (isExist) {
+            throw new BadRequestException('Email already exists');
+        }
+        const salt = bcrypt.genSaltSync(10);
+        const hashPassword = bcrypt.hashSync(user.password, salt);
+        const newUser = await this.usersService.create({
+            ...user,
+            password: hashPassword
+        })
+        return newUser;
+    }
+
 
 }
